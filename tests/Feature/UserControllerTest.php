1be8f8f0c52ec5a8664fecf9218e45c0ced26bba
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-30 05:57:12 +0700

    Membuat Login Action

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -13,4 +13,27 @@ class UserControllerTest extends TestCase
         $this->get('/login')
             ->assertSeeText("Login");
     }
+
+    public function testLoginSuccess()
+    {
+        $this->post('/login', [
+            "user"  => "matt",
+            "password" => "rahasia"
+        ])->assertRedirect("/")
+            ->assertSessionHas("user", "matt");
+    }
+
+    public function testLoginValidationError()
+    {
+        $this->post('/login', [])
+            ->assertSeeText("User or Password is required!");
+    }
+
+    public function testLoginFailed()
+    {
+        $this->post('/login', [
+            "user"  => "matt",
+            "password" => "salah"
+        ])->assertSeeText("User or password not valid!");
+    }
 }
